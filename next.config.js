81d21f30f0e13d2aae8a9b1be8dbb005d92bcf2e
blurryfacebeat <blurryfacebeat@gmail.com>
2021-12-19 20:39:45 +0700

    Add eslint and svg-loader

--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   reactStrictMode: true,
-}
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: { and: [/\.(ts)x?$/] },
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgoConfig: { plugins: [{ removeViewBox: false }] },
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+};
